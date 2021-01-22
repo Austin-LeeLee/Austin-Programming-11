a924b955a84b09da9a3db36204d5e21f386db14c
Austin-LeeLee <77767522+Austin-LeeLee@users.noreply.github.com>
2021-01-21 20:18:06 -0800

    Update Main.java

--- Expressions Assignment/src/Main.java
+++ Expressions Assignment/src/Main.java
@@ -15,17 +15,28 @@ public class Main {
         */
         //Expression 1
         System.out.println((9 - 4)+ 10/2 - (4 - 8)/3);
-        //my answer
+        System.out.println(5 + 5 + 1);
+        System.out.println(11);
+
         //Expression 2
         System.out.println(23%4 + (18 - 3 / 2));
+        System.out.println(3 + 17);
+        System.out.println(20);
 
         //Expression 3
         System.out.println(((8 - 5) + Math.pow(3,4)/5));
+        System.out.println(3 + 16.2);
+        System.out.println(19.2);
 
         //Expression 4
         System.out.println((9 % 4) - 8 + Math.pow(2,3) / 8 + (Math.sqrt(81) / 3));
+        System.out.println(1 - 8 + 1 + 3);
+        System.out.println(-3);
 
         //Expression 5
         System.out.println(((14 / 2 + 3) * Math.sin(45) + 2) - Math.pow(Math.sqrt(25), 3));
+        System.out.println(10*0.85090352453+2-125);
+        System.out.println(-114.4909647547);
     }
 }
+
