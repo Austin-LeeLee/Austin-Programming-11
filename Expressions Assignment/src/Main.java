66deb39fcdad0fabfe24d246dc96e64437ee58bc
Austin-LeeLee <77767522+Austin-LeeLee@users.noreply.github.com>
2021-03-09 20:55:00 -0800

    Add files via upload

--- Expressions Assignment/src/Main.java
+++ Expressions Assignment/src/Main.java
@@ -1,42 +1,67 @@
-public class Main {
-    public static void main(String args[]){
-        /*For the following expressions write out using code step by step how the
-        compiler will evaluate it
-        Example:
-        System.out.println(2 * 5 + 3);
-        System.out.println(10 + 3);
-        System.out.println(13);
-        Only complete one computation per line of code
-        The output for every line is always equal to the same number
-        In example above the out put should be
-        13
-        13
-        13
-        */
-        //Expression 1
-        System.out.println((9 - 4)+ 10/2 - (4 - 8)/3);
-        System.out.println(5 + 5 + 1);
-        System.out.println(11);
+import java.util.Scanner;
 
-        //Expression 2
-        System.out.println(23%4 + (18 - 3 / 2));
-        System.out.println(3 + 17);
-        System.out.println(20);
 
-        //Expression 3
-        System.out.println(((8 - 5) + Math.pow(3,4)/5));
-        System.out.println(3 + 16.2);
-        System.out.println(19.2);
+class Main {
+    public static void main(String args[]) {
 
-        //Expression 4
-        System.out.println((9 % 4) - 8 + Math.pow(2,3) / 8 + (Math.sqrt(81) / 3));
-        System.out.println(1 - 8 + 1 + 3);
-        System.out.println(-3);
 
-        //Expression 5
-        System.out.println(((14 / 2 + 3) * Math.sin(45) + 2) - Math.pow(Math.sqrt(25), 3));
-        System.out.println(10*0.85090352453+2-125);
-        System.out.println(-114.4909647547);
+        System.out.println("Choose ROCK, PAPER, or SCISSORS. '0' for rock, '1' for paper, '2' for scissors");
+        Scanner scan = new Scanner(System.in);
+        int computerChoice = (int) (Math.random() * 3);
+        int playerChoice = Integer.parseInt(scan.next());
+
+        System.out.print("Computer Choice: " + computerChoice);
+        System.out.println("    Player Choice : " + playerChoice);
+
+        //Checking to see if player and computer (AI) chooses the same thing.
+        if (computerChoice == playerChoice)
+            System.out.println("Draw!");
+
+        //If player chooses rock
+        if ((playerChoice == 0) && (computerChoice == 1)) {
+            System.out.println("Computer Wins!");
+        } else if ((playerChoice == 0) && (computerChoice == 2))
+            System.out.println("Player wins! ");
+
+        //If player chooses paper
+        if ((playerChoice == 1) && (computerChoice == 0)) {
+            System.out.println("Player wins!");
+        } else if ((playerChoice == 1) && (computerChoice == 2))
+            System.out.println("Computer Wins!");
+
+        //If player chooses scissors
+        if ((playerChoice == 2) && (computerChoice == 0))
+            System.out.println("Computer Wins!");
+        else if ((playerChoice == 2) && (computerChoice == 1))
+            System.out.println("Player wins!");
+
+        //Checking to see if player inserts a valid number that is less than 3
+        if (!(playerChoice < 3))
+            System.out.println( playerChoice + " is not a valid choice, please play again. ");
+
+
+
+
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
